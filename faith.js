69ecf5a8174be544116a4b878e8b7304ebda6364
FnvaAcademy <67489226+FnvaAcademy@users.noreply.github.com>
2020-06-26 21:29:02 -0700

    first commit

---
+++ faith.js
@@ -0,0 +1,10 @@
+function menu(){
+    $('#menu').click(function(){
+        $("#menuContent").slideToggle(1000);
+    })
+}
+function information(){
+    alert("still unnder construction :) ");
+}
+information()
+menu();
